18839a612bfa8e19c73b8814cd248a6a8d0f79f6
Thorsten Hans <thorsten.hans@gmail.com>
2011-01-27 00:20:05 +0100

    Added missing interfaces

---
+++ DotNetRocks.FluentSPRibbon/IRibbonElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace DotNetRocks.FluentSPRibbon
+{
+    public interface IRibbonElement : IXmlSerializable
+    {
+        String Id { get; }
+        String ToXml();
+    }
+}
---
+++ DotNetRocks.FluentSPRibbon/IRibbonElementContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DotNetRocks.FluentSPRibbon
+{
+    public interface IRibbonElementContainer<TContainer, TChildControls>
+    {
+        TChildControls this[String id] { get; }
+        TContainer With(Func<TChildControls> expression);
+    }
+}
