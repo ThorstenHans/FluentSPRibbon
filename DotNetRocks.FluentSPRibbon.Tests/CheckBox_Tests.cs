705084394785ee868b525fb63be650c3166018b1
Thorsten Hans <thorsten.hans@gmail.com>
2011-01-30 19:03:11 +0100

    Added tests for CheckBox and Color

---
+++ DotNetRocks.FluentSPRibbon.Tests/CheckBox_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DotNetRocks.FluentSPRibbon.Tests
+{
+    [TestFixture]
+    public class CheckBox_Tests
+    {
+        [Test]
+        public void Create_Should_Create_A_New_Instance()
+        {
+            var sut = Create<CheckBox>.Instance("MyCheckBox");
+
+            Assert.IsNotNull(sut);
+            Assert.IsInstanceOf<CheckBox>(sut);
+            Assert.AreEqual("MyCheckBox",sut.Id);
+        }
+
+        [Test]
+        public void PassedParameter_Should_Be_Stored_In_OriginalId()
+        {
+            var sut = new CheckBox("MyCheckBox");
+            Assert.AreEqual("MyCheckBox",sut.OriginalId);
+        }
+
+        [Test]
+        public void IsIdProvider_Should_Be_True()
+        {
+            var sut = new CheckBox("MyCheckBox");
+            Assert.IsTrue(sut.IsIdProvider);
+        }
+
+        [Test]
+        public void Id_Should_Be_Compsited_If_Parent_Is_Present()
+        {
+            var sut = new CheckBox("CheckBox1");
+            var group = new Group("ActionGroup1");
+            var tab = new Tab("CommonTab");
+            var ribbon = new Ribbon("MyRibbon");
+            ribbon.With(() => tab
+                                  .With(() => group
+                                                  .With(() => sut)));
+
+            Assert.AreEqual("MyRibbon.CommonTab.ActionGroup1.CheckBox1", sut.Id);
+        }
+    }
+}
--- DotNetRocks.FluentSPRibbon.Tests/Color_Tests.cs
+++ DotNetRocks.FluentSPRibbon.Tests/Color_Tests.cs
@@ -1,10 +1,28 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace DotNetRocks.FluentSPRibbon.Tests
 {
+    
     [TestFixture]
     public class Color_Tests
     {
+        [Test]
+        public void Create_Should_Create_A_New_Instance()
+        {
+            var sut = Create<Color>.Instance("MyColor");
+            Assert.IsNotNull(sut);
+            Assert.IsInstanceOf<Color>(sut);
+        }
+
+        [Test]
+        public void IsIdProvider_Should_Be_False()
+        {
+            var sut = new Color("MyColor");
+
+            Assert.IsFalse(sut.IsIdProvider);
+        }
+
         [Test]
         public void Id_Should_Not_Be_Exported_To_Xml()
         {
@@ -12,6 +30,28 @@ namespace DotNetRocks.FluentSPRibbon.Tests
             sut.SetProperty(ColorProperty.Title, "Red");
             var actual = sut.ToXml();
             Assert.IsFalse(actual.Contains("Id"));
+        }
+
+        [Test]
+        public void SetProperty_Should_Store_Value()
+        {
+            var sut = new Color("My Color");
+            sut.SetProperty(ColorProperty.Title, "My Color");
+
+            Assert.AreEqual("My Color", sut.GetProperty(ColorProperty.Title));
+        }
+
+        [Test]
+        public void SetProperties_Should_Store_MultipleValues()
+        {
+            var sut = new Color("MyColor");
+            sut.SetProperties(new Dictionary<ColorProperty, string>()
+                                  {
+                                      {ColorProperty.Title, "My Color"},
+                                      {ColorProperty.DisplayColor, "Red"}
+                                  });
+            Assert.AreEqual("My Color", sut.GetProperty(ColorProperty.Title));
+            Assert.AreEqual("Red", sut.GetProperty(ColorProperty.DisplayColor));
         }
     }
 }
