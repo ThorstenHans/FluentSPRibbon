12bfa1fb99f7387e9c6be1623d5a5be2defe9595
Thorsten Hans <thorsten.hans@gmail.com>
2011-06-16 14:54:55 +0200

    QATProperties und ControlsProperties added, Vererbung umgeleitet auf RibbonElement<T,TPropertyEnum>

---
+++ DotNetRocks.FluentSPRibbon/PropertyEnums/ControlsProperty.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetRocks.FluentSPRibbon
+{
+    public enum ControlsProperty
+    {
+        [ImageProvider, UrlProvider]
+        ToolTipImage32by32,
+        ToolTipImage32by32Class,
+        ToolTipImage32by32Left,
+        ToolTipImage32by32Top,
+        [TextProvider]
+        ToolTipTitle,
+        [TextProvider]
+        ToolTipDescription,
+        [TextProvider]
+        ToolTipHelpKeyWord,
+        ToolTipShortcutKey
+    }
+}
---
+++ DotNetRocks.FluentSPRibbon/PropertyEnums/QATProperty.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetRocks.FluentSPRibbon
+{
+    public enum QATProperty
+    {
+        [ImageProvider,UrlProvider]
+        ImageDownArrow,
+        ImageDownArrowClass,
+        ImageDownArrowLeft,
+        ImageDownArrowTop,
+        [ImageProvider, UrlProvider]
+        ImageSideArrow,
+        ImageSideArrowClass,
+        ImageSideArrowLeft,
+        ImageSideArrowTop,
+        [ImageProvider, UrlProvider]
+        ImageUpArrow,
+        ImageUpArrowClass,
+        ImageUpArrowLeft,
+        ImageUpArrowTop
+
+    }
+}
